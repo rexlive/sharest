c1bed1c3863db4c8527cb37e268661398e0d297f
rexlive <rexlive@gmail.com>
2025-03-21 06:01:24 -0700

    new

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "rex Blog";
+export const SITE_TITLE = "Rex Blog";
 export const SITE_DESCRIPTION = "share";
